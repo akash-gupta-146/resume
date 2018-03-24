fc50e0d65daf82d5e0e10a991c4833a27471e71f
akash-gupta-146 <ag.akash007@gmail.com>
2018-03-25 02:30:27 +0530

    added intro page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCd29pcYo9q147sDlltlnWv25e7qABqk8s",
+  authDomain: "portfolio-akash.firebaseapp.com",
+  databaseURL: "https://portfolio-akash.firebaseio.com",
+  projectId: "portfolio-akash",
+  storageBucket: "",
+  messagingSenderId: "786288604824"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,                 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/intro/intro.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Intro{
+  name:string;
+  title:string;
+  summary:string;
+}
+
+interface SocialLinks{
+  name:string;
+  href:string;
+  icon:string;
+}
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.scss']
+})
+export class IntroComponent implements OnInit {
+
+  introDoc: AngularFirestoreDocument<Intro>;
+  intro: Observable<Intro>;
+  summary: Intro;
+
+  socialDoc: AngularFirestoreDocument<SocialLinks>;
+  social: Observable<SocialLinks>;
+  socialLinks: SocialLinks;
+
+  testArray:any[] = [];
+
+  constructor(private afs: AngularFirestore) { }
+
+  ngOnInit() {
+    this.getSummary();
+    this.getSocialDetails();
+  }
+
+  getSummary(){
+    this.introDoc = this.afs.doc('intro/zvg54q7L4Ci4Hyo2c4Lh');
+    this.intro = this.introDoc.valueChanges();
+    this.intro.subscribe(notes => {
+      this.summary = notes;
+    })
+  }
+  getSocialDetails(){
+    this.socialDoc = this.afs.doc('social-links/Ai9CBiYUcsDiYnmIu1M4')
+    this.social = this.socialDoc.valueChanges();
+    this.social.subscribe(socialLink => {
+      this.socialLinks = socialLink;
+      // console.log(this.socialLinks);
+      Object.keys(this.socialLinks).forEach((element:any,index) => {
+        this.testArray[index] = element;
+      });
+      // console.log(this.testArray);
+    });
+  }
+
+}
