17ea848e2f61c4c36eae9cc9f5986979ac953701
akash-gupta-146 <ag.akash007@gmail.com>
2018-04-01 18:48:12 +0530

    Completed services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
+
+interface Services{
+  title:string;
+  icon:string;
+  description:string;
+  points:string[];
+}
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
@@ -7,9 +16,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  serviceCollection: AngularFirestoreCollection<Services>;
+  receivedServices: Observable<Services[]>;;
+  services:Services[];
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit() {
+    this.getServices();
+  }
+
+  getServices(){
+    this.serviceCollection = this.afs.collection('services');
+    this.receivedServices = this.serviceCollection.valueChanges();
+    this.receivedServices.subscribe(response =>{
+      this.services = response;
+      console.log(response);
+    })
   }
 
 }
